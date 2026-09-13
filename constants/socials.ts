5311000d9cedd6e3003a9634f321f1e16d63740b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-09-13 19:46:56 +0500

    feat: add Gmail and Telegram icons to the project and update socials list

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,5 +1,7 @@
 import { GitHub } from "@/components/icons/github";
+import { Gmail } from "@/components/icons/gmail";
 import { NPM } from "@/components/icons/npm";
+import { Telegram } from "@/components/icons/telegram";
 import type { SVGProps } from "react";
 
 interface Socials {
@@ -18,5 +20,15 @@ export const socials: Socials[] = [
     name: "NPM",
     link: "https://www.npmjs.com/~david021106",
     icon: NPM,
+  },
+  {
+    name: "Telegram",
+    link: "https://t.me/whoisdave02",
+    icon: Telegram,
+  },
+  {
+    name: "Gmail",
+    link: "mailto:asrorxonovdovudxon@gmail.com",
+    icon: Gmail,
   },
 ];
